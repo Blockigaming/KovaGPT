f75c241d1cd46b6b8ccfd0997f995bd426832f40
Zachary Block <zacharylblock@gmail.com>
2026-08-31 20:37:53 -0400

    fix: pin latest Stripe Dahlia API

--- src/lib/stripe.server.ts
+++ src/lib/stripe.server.ts
@@ -17,7 +17,7 @@ export function createStripeClient(env: StripeEnv): Stripe {
   const connectionApiKey = getConnectionApiKey(env);
 
   return new Stripe(connectionApiKey, {
-    apiVersion: "2026-07-29.dahlia",
+    apiVersion: "2026-08-26.dahlia",
   });
 }
 
