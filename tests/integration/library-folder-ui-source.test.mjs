1647d9bd8315a3bf0c992504995e5652d833eb70
Zachary Block <zacharylblock@gmail.com>
2026-09-03 22:33:29 -0700

    style: format folder availability assertion

--- tests/integration/library-folder-ui-source.test.mjs
+++ tests/integration/library-folder-ui-source.test.mjs
@@ -95,7 +95,10 @@ test("page refresh cannot invalidate an active folder mutation", () => {
   assert.match(library, /onBusyChange=\{setFolderBusy\}/);
   assert.match(library, /if \(folderBusy\) return/);
   assert.match(library, /disabled=\{loading \|\| folderBusy\}/);
-  assert.match(folders, /const folderStateUnavailable = loading \|\| Boolean\(error\) \|\| !foldersLoaded/);
+  assert.match(
+    folders,
+    /const folderStateUnavailable = loading \|\| Boolean\(error\) \|\| !foldersLoaded/,
+  );
   assert.match(folders, /onClick=\{onRefresh\}/);
   assert.match(library, /onRefresh=\{refreshLibrary\}/);
 });
